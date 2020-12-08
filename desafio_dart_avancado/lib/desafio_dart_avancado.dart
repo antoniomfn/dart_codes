a82591740fe415ee714d3977266d4a051fac585d
Antonio <tonimfn@gmail.com>
2020-12-08 08:55:55 -0300

    Atualizando desafio avançado

--- desafio_dart_avancado/lib/desafio_dart_avancado.dart
+++ desafio_dart_avancado/lib/desafio_dart_avancado.dart
@@ -2,15 +2,20 @@ import 'package:mysql1/mysql1.dart';
 import 'package:desafio_dart_avancado/api/api_cidades.dart';
 import 'package:desafio_dart_avancado/api/api_estados.dart';
 
-void run() async {
+Future<void> run() async {
   // Inicia a conexão com o MySQL
   var conn = await getConnection();
 
   // Cria as tabelas
   await createTables(conn);
 
-  // Inicia a função de incremento
-  await increment(conn);
+  // Incrementa estados e cidades
+  await incrementEstados(conn);
+
+  // Fecha a conexão
+  await conn.close();
+
+  print('O processo foi concluído com sucesso.');
 }
 
 Future<MySqlConnection> getConnection() async {
@@ -24,38 +29,52 @@ Future<MySqlConnection> getConnection() async {
 }
 
 // Função de criar tabelas
-void createTables(MySqlConnection connection) async {
-  await connection.query('''CREATE TABLE IF NOT EXISTS estados (
+void createTables(MySqlConnection conn) async {
+  // Cria a tabela de estados
+  await conn.query('''CREATE TABLE estados (
                         id int primary key not null, 
                         estado varchar(100) not null, 
                         sigla varchar(2) not null);
-                   ''');
-  await connection.query('''CREATE TABLE IF NOT EXISTS cidades (
+                  ''');
+  print("Tabela 'estados' criada.");
+
+  // Cria a tabela de cidades
+  await conn.query('''CREATE TABLE cidades (
                         id int primary key not null,
                         cidade varchar(100) not null,
                         estado_id int not null,
                         foreign key (estado_id) references estados(id));
-                   ''');
-
-  print('Tabelas criadas com sucesso.');
+                  ''');
+  print("Tabela 'cidades' criada.");
 }
 
 // Função de incremento de estados
-void increment(MySqlConnection conn) async {
+void incrementEstados(MySqlConnection conn) async {
   // Conecta com a API para pegar os estados
   var estados = await getEstados();
+
   // Adiciona os estados no banco
-  estados.forEach((e) async {
+  await Future.forEach(estados, (e) async {
     // Insert de estados
     await conn.query('INSERT INTO estados (id, estado, sigla) VALUES (?, ?, ?)',
         [e.id, e.nome, e.sigla]);
-    // Conecta com a API para pegar as cidades
-    var cidades = await getCidades(e.id);
-    cidades.forEach((c) {
-      // Insert de cidades
-      conn.query('INSERT INTO cidades (id, cidade, estado_id) VALUES (?, ?, ?)',
-          [c.id, c.nome, e.id]);
-    });
+    print('Estado ${e.nome} foi adicionado');
+
+    await incrementCidades(conn, e.id);
     print('Todas as cidades do estado ${e.nome} foram adicionadas');
+  });
+}
+
+// Função de incremento de cidades
+void incrementCidades(MySqlConnection conn, int estado_id) async {
+  // Conecta com a API para pegar as cidades
+  var cidades = await getCidades(estado_id);
+
+  // Adiciona as cidades no banco
+  await Future.forEach(cidades, (c) async {
+    // Insert de cidades
+    await conn.query(
+        'INSERT INTO cidades (id, cidade, estado_id) VALUES (?, ?, ?)',
+        [c.id, c.nome, estado_id]);
   });
 }
